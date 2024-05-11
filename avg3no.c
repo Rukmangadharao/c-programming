aea860d4764a827060c67199813a5910e710d58e
Rukmangadharao <rukmangadharao6242@gmail.com>
2024-05-11 11:52:24 +0530

    Add files via upload

---
+++ avg3no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+float avg;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+printf("enter the third nymber");
+scanf("%d",&c);
+avg=(a+b+c)/3;
+printf("the avg value",avg);
+}
